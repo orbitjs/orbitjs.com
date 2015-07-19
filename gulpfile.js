1cd655adb4634bda629897570e3158ac1a2ef037
Jmeas <jellyes2@gmail.com>
2015-07-19 00:31:43 -0400

    Add Gulp task to generate API docs.
    Resolves #21

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ gulp.task('clean-css', function(cb) {
   del('./css/style.css', cb);
 });
 
+// Remove the existing API docs file
+gulp.task('clean-api-docs', function(cb) {
+  del('./_data/orbitjs_api.json', cb);
+});
+
 // Compile our Stylus
 gulp.task('stylus', function() {
   return gulp.src('./css/stylus/index.styl')
@@ -15,8 +20,21 @@ gulp.task('stylus', function() {
     .pipe(gulp.dest('./css'))
 });
 
+// Generate the API docs from the source library. For this
+// to work, the main orbit repo must be in a sibling directory
+// to this repository.
+gulp.task('generate-api-docs', function() {
+  return gulp.src('../orbit.js/lib/**/*.js')
+    .pipe($.yuidoc.parser())
+    .pipe($.rename('orbitjs_api.json'))
+    .pipe(gulp.dest('./_data'));
+});
+
 // Build the site
 gulp.task('build', ['clean-css', 'stylus']);
+
+// Get the API docs in order
+gulp.task('docs', ['clean-api-docs', 'generate-docs']);
 
 // An alias of build
 gulp.task('default', ['build']);
