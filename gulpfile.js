8ef4a9014d2fd587187581efe89a1bdfe363cd49
Jmeas <jellyes2@gmail.com>
2015-07-19 14:32:40 -0400

    Add a Gulp task to transform the API data.
    Resolves #16

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
+const loadPlugins = require('gulp-load-plugins');
 const del = require('del');
+const _ = require('lodash');
+
+// Load the Gulp plugins under $
+const $ = loadPlugins({
+  rename: {
+    'gulp-json-transform': 'transformJson'
+  }
+});
 
 // Remove the built CSS file
 gulp.task('clean-css', function(cb) {
@@ -10,6 +18,11 @@ gulp.task('clean-css', function(cb) {
 // Remove the existing API docs file
 gulp.task('clean-api-docs', function(cb) {
   del('./_data/orbitjs_api.json', cb);
+});
+
+// Remove the generated API docs file
+gulp.task('clean-generated-api', function(cb) {
+  del('./_data/api_data.json')
 });
 
 // Compile our Stylus
@@ -23,10 +36,47 @@ gulp.task('stylus', function() {
 // Generate the API docs from the source library. For this
 // to work, the main orbit repo must be in a sibling directory
 // to this repository.
-gulp.task('generate-api-docs', function() {
+gulp.task('fetch-api-docs', function() {
   return gulp.src('../orbit.js/lib/**/*.js')
     .pipe($.yuidoc.parser())
     .pipe($.rename('orbitjs_api.json'))
+    .pipe(gulp.dest('./_data'));
+});
+
+// After the API docs have been fetched, they need to be transformed
+// for our templates to render the proper HTML
+gulp.task('transform-api-docs', ['fetch-api-docs'], function() {
+
+  // Determine whether a YUIDoc "class" definition is a namespace or not
+  var filter = function(definition) {
+    return definition.namespace;
+  };
+
+  return gulp.src('./_data/orbitjs_api.json')
+    .pipe($.transformJson(function(data) {
+      var classes = data.classes;
+
+      // Find the namespaces
+      var namespaces = _.reject(classes, filter);
+
+      // Create an object of namespaces with the classes
+      // nested within them.
+      return _.chain(classes)
+        .filter(filter)
+        .groupBy('namespace')
+        .mapValues(function(group, name) {
+          
+          // Locate the correct namespace given the name of the group
+          var namespace = _.findWhere(namespaces, {name: name});
+
+          // Extend that namespace with the group
+          return _.extend(namespace, {
+            classes: _.sortBy(group, 'name')
+          });
+        })
+        .value();
+    }))
+    .pipe($.rename('api_data.json'))
     .pipe(gulp.dest('./_data'));
 });
 
@@ -34,7 +84,7 @@ gulp.task('generate-api-docs', function() {
 gulp.task('build', ['clean-css', 'stylus']);
 
 // Get the API docs in order
-gulp.task('docs', ['clean-api-docs', 'generate-docs']);
+gulp.task('docs', ['clean-api-docs', 'clean-generated-api', 'transform-api-docs']);
 
 // An alias of build
 gulp.task('default', ['build']);
